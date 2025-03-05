b9cc0850f1e04870b4862c0bd738192dd37e7233
Caolan Maguire <caolanmichaelmaguire@gmail.com>
2025-03-05 19:01:42 +0000

    back again

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const NodeCache = require('node-cache');
 const http = require('http');
-// const { Server } = require('socket.io');
+const { Server } = require('socket.io');
 
 const app = express();
 
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//     cors: { origin: '*' } // Allow all origins
-// });
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: '*' } // Allow all origins
+});
 
 app.use(cors()); // Enable CORS for all requests
 app.use(express.json());
@@ -64,28 +64,28 @@ const handleDBError = (err, res) => {
     res.status(500).json({ message: 'Database error', error: err });
 };
 
-// // Socket.IO connection handling
-// io.on('connection', (socket) => {
-//     console.log('A user connected');
+// Socket.IO connection handling
+io.on('connection', (socket) => {
+    console.log('A user connected');
 
-//     socket.on('disconnect', () => {
-//         console.log('User disconnected');
-//     });
-// });
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
 
-// app.post('/update-number', (req, res) => {
-//     const { vid, number_value } = req.body;
+app.post('/update-number', (req, res) => {
+    const { vid, number_value } = req.body;
 
-//     if (!vid || number_value === undefined) {
-//         return res.status(400).json({ message: 'Missing required fields: vid, number_value' });
-//     }
+    if (!vid || number_value === undefined) {
+        return res.status(400).json({ message: 'Missing required fields: vid, number_value' });
+    }
 
-//     // Emit the number update to all connected clients
-//     io.emit('numberUpdated', { vid, number_value });
+    // Emit the number update to all connected clients
+    io.emit('numberUpdated', { vid, number_value });
 
-//     res.status(200).json({ message: 'Number updated successfully', number_value });
-// });
+    res.status(200).json({ message: 'Number updated successfully', number_value });
+});
 
 // Register route for creating a new user
 app.post('/register', async (req, res) => {

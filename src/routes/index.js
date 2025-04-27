0b426c7411aa0f818e66b192bec4ef8dc3a2ba54
Caolan Maguire <caolanmichaelmaguire@gmail.com>
2025-04-27 17:25:03 +0100

    changed endpoint names

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const authRoutes = require('./auth');
 const vehicleRoutes = require('./vehicle');
 const journeyRoutes = require('./journey');
 
-router.use('/auth', authRoutes);
+router.use('/', authRoutes);
 router.use('/vehicles', vehicleRoutes);
 router.use('/journeys', journeyRoutes);
 

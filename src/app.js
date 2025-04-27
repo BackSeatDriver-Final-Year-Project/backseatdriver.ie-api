fea987b42be4f1d41b25568689e6a9e84a353c92
Caolan Maguire <caolanmichaelmaguire@gmail.com>
2025-04-27 19:52:00 +0100

    updated cors policy

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(cors({
     origin: '*', // Allow any origin
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
 
